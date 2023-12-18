2ee40439d5cd2213df00999608e41b70c78be81a
Md.Sumsul Arifin <99475443+SumsulArifin@users.noreply.github.com>
2023-12-18 15:24:01 +0600

    p
    p

---
+++ salesOfPoint/src/main/java/com/example/salesOfPoint/SalesOfPointApplication.java
@@ -0,0 +1,13 @@
+package com.example.salesOfPoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesOfPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesOfPointApplication.class, args);
+	}
+
+}
